746de1890a987ad8e926ed1c06bd862a98f7f798
DaniilKurandin <kurandin1995@mail.ru>
2021-06-17 12:13:14 +0500

    MASTER-1: start add java_hibernate

---
+++ src/main/java/com/itvdn/AnimalDAO.java
@@ -0,0 +1,26 @@
+package com.itvdn;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class AnimalDAO {
+    public Animal getAnimal(int id) {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(Animal.class, id);
+    }
+
+    public void update(Animal animal) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.update(animal);
+        tx1.commit();
+        session.close();
+    }
+
+    public void delete(Animal animal) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.delete(animal);
+        tx1.commit();
+        session.close();
+    }
+}
---
+++ src/main/java/com/itvdn/Main.java
@@ -0,0 +1,30 @@
+package com.itvdn;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Tvarina", 11, true);
+
+        SessionFactory factory = new Configuration()
+                .configure()
+                .buildSessionFactory();
+
+        Session session = factory.openSession();
+        session.beginTransaction();
+        session.save(animal);
+        session.flush();
+        session.getTransaction().commit();
+
+        //       List<Animal> list = session.createQuery("from Animal", Animal.class).list();
+
+        session.close();
+        factory.close();
+
+//        for(Animal animal: list) {
+//            System.out.println(animal);
+//        }
+    }
+}
